06e5684283fca0e5edc48ad28ee56fc2dfd14451
ryancch <ryanchan.cch@gmail.com>
2020-02-02 15:50:16 -0800

    Changed main to test Insertion Selection and Deletion

--- __main__.py
+++ __main__.py
@@ -32,14 +32,14 @@ for i in range(0, 10000):
     query.update(choice(keys), *(choice(update_cols)))
 update_time_1 = process_time()
 print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)
-
+"""
 # Measuring Select Performance
 select_time_0 = process_time()
 for i in range(0, 10000):
     query.select(choice(keys), [1, 1, 1, 1, 1])
 select_time_1 = process_time()
 print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0)
-
+"""
 # Measuring Aggregate Performance
 agg_time_0 = process_time()
 for i in range(0, 10000, 100):
